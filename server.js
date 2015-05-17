8a67ec29f52cb81b6214bd0e5f609c31103b00f4
Seth Lakowske <lakowske@gmail.com>
2015-05-17 13:11:30 -0500

    register the service

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require('http');
 var mail = require('./');
+var register = require('service-directory').register;
 
 var port = parseInt(process.argv[2], 10);
 
-http.createServer(function(req, res) {
-    var mailPs = mail('mail service', 'lakowske@gmail.com');
-    req.pipe(mailPs.stdin);
-}).listen(port);
+//register the email service with a service directory
+register('http://localhost:1111', 'email', 'localhost', port, {}, function () {
+    http.createServer(function(req, res) {
+        var mailPs = mail('mail service', 'lakowske@gmail.com');
+        req.pipe(mailPs.stdin);
+    }).listen(port);
+})
