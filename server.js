ec9b37323eb8ad0f20fe9b4e7a83c36d7583d93b
Seth <lakowske@gmail.com>
2015-05-23 12:19:59 -0500

    when the user has finished piping the email, close the connection

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ var port = parseInt(process.argv[2], 10);
 http.createServer(function(req, res) {
     var mailPs = mail('mail service', 'lakowske@gmail.com');
     req.pipe(mailPs.stdin);
+    req.on('end', function() {
+        res.end('email sent');
+    })
 }).listen(port);
