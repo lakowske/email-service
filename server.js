428242fe857d1d08da5f609e41a4fd9702d8099f
Seth <lakowske@gmail.com>
2015-05-25 18:59:10 -0500

    better server

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ var mail = require('./');
 var port = parseInt(process.argv[2], 10);
 
 http.createServer(function(req, res) {
-    var mailPs = mail('mail service', 'lakowske@gmail.com');
+
+    var subject = 'mail service'
+    if(req.headers && req.headers.subject) {
+        subject = req.headers.subject;
+    }
+
+    var mailPs = mail(subject, 'lakowske@gmail.com');
     req.pipe(mailPs.stdin);
+    var msg = '';
+
+    req.on('data', function(data) {
+        console.log('request data');
+        msg += data;
+    });
+
     req.on('end', function() {
-        res.end('email sent');
+        console.log('request ended');
+        console.log(msg);
+        res.end('sent: ' + msg);
     })
 }).listen(port);
