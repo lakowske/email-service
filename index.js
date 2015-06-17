824ceb8b38667ac1cbb4c3e8dd1a46e3f8f80872
Seth Lakowske <lakowske@gmail.com>
2015-06-17 08:52:02 -0500

    add ssmtp configuration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var child_process = require('child_process');
 
 function mail(subject, recepient) {
 
-    var mail = child_process.spawn('mail', ['-s', subject, recepient]);
+    var mail = child_process.spawn('ssmtp', ['-s', subject, recepient]);
 
     return mail;
 }
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var mail = require('./');
-var register = require('service-directory').register;
 
 var port = parseInt(process.argv[2], 10);
 
 //register the email service with a service directory
-register('http://localhost:1111', 'email', 'localhost', port, {}, function () {
-    http.createServer(function(req, res) {
-        var mailPs = mail('mail service', 'lakowske@gmail.com');
-        req.pipe(mailPs.stdin);
-    }).listen(port);
-})
+
+http.createServer(function(req, res) {
+    var mailPs = mail('mail service', 'lakowske@gmail.com');
+    req.pipe(mailPs.stdin);
+}).listen(port);
